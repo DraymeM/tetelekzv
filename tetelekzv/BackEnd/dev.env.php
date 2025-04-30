b329a729d44d51979363cdd77b4945f1d8ce1135
DraymeM <mixcrafting.d@gmail.com>
2025-04-30 18:29:19 +0200

    remove

--- tetelekzv/BackEnd/dev.env.php
+++
@@ -1,7 +0,0 @@
-<?php
-return [
-  'DB_HOST' => 'localhost',
-  'DB_NAME' => 'danielmarkus',
-  'DB_USER' => 'danielmarkus',
-  'DB_PASS' => 'your_local_password',
-];
--- tetelekzv/BackEnd/env.php
+++
@@ -1,7 +0,0 @@
-<?php
-return [
-  'DB_HOST' => 'mysql.caesar.elte.hu',
-  'DB_NAME' => 'danielmarkus',
-  'DB_USER' => 'danielmarkus',
-  'DB_PASS' => 'VnwqWXbc87uWrMhb',
-];
